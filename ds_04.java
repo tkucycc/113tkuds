728219eee1a2c85cabafabd37ae7f74f42b49505
tkucycc <145464164+tkucycc@users.noreply.github.com>
2025-06-29 16:04:04 +0800

    作業四

---
+++ ds_04.java
@@ -0,0 +1,10 @@
+public class ds_04 {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        System.out.println("一維陣列的元素為：");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("第 " + (i + 1) + " 個元素：" + numbers[i]);
+        }
+    }
+}
