b309252d6a13fae98590da5c920fc65f313c8cd0
tkucycc <145464164+tkucycc@users.noreply.github.com>
2025-06-29 16:05:23 +0800

    作業七

---
+++ ds_07.java
@@ -0,0 +1,17 @@
+public class ds_07 {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println("3x3 二維陣列的內容為：");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println(); // 換行
+        }
+    }
+}
